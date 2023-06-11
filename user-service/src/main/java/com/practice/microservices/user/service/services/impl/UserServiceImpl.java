00ad1086c352786ac1c00d25bae2f10064ecf717
Max Dev <shoebmoh13@gmail.com>
2023-06-11 23:05:47 +0530

    service to service calls

--- user-service/src/main/java/com/practice/microservices/user/service/services/impl/UserServiceImpl.java
+++ user-service/src/main/java/com/practice/microservices/user/service/services/impl/UserServiceImpl.java
@@ -1,20 +1,38 @@
 package com.practice.microservices.user.service.services.impl;
 
+import com.practice.microservices.user.service.entities.Hotel;
+import com.practice.microservices.user.service.entities.Rating;
 import com.practice.microservices.user.service.entities.User;
 import com.practice.microservices.user.service.exceptions.ResourceNotFoundException;
+import com.practice.microservices.user.service.external.services.HotelService;
 import com.practice.microservices.user.service.repository.UserRepository;
 import com.practice.microservices.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private HotelService hotelService;
+
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Override
     public User saveUser(User user) {
@@ -30,6 +48,23 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUser(String userId) {
-        return userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User Not found With Id: "+ userId));
+        User user = userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("User Not found With Id: " + userId));
+        Rating[] ratingsOfUser = restTemplate.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), Rating[].class);
+        logger.info("{} ", ratingsOfUser);
+
+        List<Rating> ratings = Arrays.asList(ratingsOfUser).stream().collect(Collectors.toList());
+
+        List<Rating> ratingList = ratings.stream().map(rating -> {
+//            ResponseEntity<Hotel> forEntity = restTemplate.getForEntity("http://HOTEL-SERVICE/hotels/" + rating.getHotelId(), Hotel.class);
+//            Hotel hotel = forEntity.getBody();
+//            logger.info("response status code {}",forEntity.getStatusCode());
+            Hotel hotel = hotelService.getHotel(rating.getHotelId());
+            rating.setHotel(hotel);
+            return rating;
+        }).collect(Collectors.toList());
+
+
+        user.setRatings(ratingList);
+        return user;
     }
 }
